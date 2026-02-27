47e045e61174cf3304232a8c72119cea4f7a3465
Mehmet Burak Altıparmak <mburakaltiparmak@gmail.com>
2026-02-27 23:55:22 +0300

    refactor(templates): remove deprecated redux-logger from store configuration

--- templates/admin/store/index.ts
+++ templates/admin/store/index.ts
@@ -1,13 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    process.env.NODE_ENV !== 'production'
-      ? getDefaultMiddleware().concat(logger)
-      : getDefaultMiddleware(),
 });
 
 // Inferred types — export and use via store/hooks.ts
